5b9d2ea3e0ac3fdf788f8dd6d5511655208fcaa8
sivamedam <116708418+sivamedam@users.noreply.github.com>
2022-11-14 18:33:56 +0530

    Delete remove_string.sh

--- remove_string.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-sed -i -e '/index/d' git_commits_log8.csv
-sed -i -e '/diff/d' git_commits_log8.csv
-sed -i -e '/@@/d' git_commits_log8.csv
-sed -i -e '/deleted/d' git_commits_log8.csv
-sed -i -e '/^$/d' git_commits_log8.csv
